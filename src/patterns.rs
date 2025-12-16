b97a5fa7d6837792991e05516a8795d7f36d72e8
Graham Kelly <gkgoat6700@gmail.com>
2025-12-15 21:45:45 -0500

    patterns

--- src/patterns.rs
+++ src/patterns.rs
@@ -1,1 +1,27 @@
+use impl_trait_for_tuples::impl_for_tuples;
+
 use crate::*;
+pub trait PatternMut {
+    type Result;
+    fn match_mut(&self, expr: &mut Expr) -> Option<Self::Result>;
+}
+pub trait Pattern: PatternMut {
+    fn r#match(&self, expr: &Expr) -> Option<Self::Result>;
+}
+#[impl_for_tuples(1, 12)]
+impl PatternMut for Tuple {
+    for_tuples!(type Result = ( #( Tuple::Result ),* ););
+    fn match_mut(&self, expr: &mut Expr) -> Option<Self::Result> {
+        Some(for_tuples!((
+            #( self.Tuple.match_mut(expr)? ),*
+        )))
+    }
+}
+#[impl_for_tuples(1, 12)]
+impl Pattern for Tuple {
+    fn r#match(&self, expr: &Expr) -> Option<Self::Result> {
+        Some(for_tuples!((
+            #( self.Tuple.r#match(expr)? ),*
+        )))
+    }
+}
