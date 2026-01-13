fec19991b8182ab154142d1b2118714aa2deb85a
Graham Kelly <gkgoat6700@gmail.com>
2026-01-12 23:18:48 -0500

    arbitrary patterns

--- src/patterns.rs
+++ src/patterns.rs
@@ -1,25 +1,25 @@
 use impl_trait_for_tuples::impl_for_tuples;
 
 use crate::*;
-pub trait PatternMut {
+pub trait PatternMut<T> {
     type Result;
-    fn match_mut(&self, expr: &mut Expr) -> Option<Self::Result>;
+    fn match_mut(&self, expr: &mut T) -> Option<Self::Result>;
 }
-pub trait Pattern: PatternMut {
-    fn r#match(&self, expr: &Expr) -> Option<Self::Result>;
+pub trait Pattern<T>: PatternMut<T> {
+    fn r#match(&self, expr: &T) -> Option<Self::Result>;
 }
 #[impl_for_tuples(1, 12)]
-impl PatternMut for Tuple {
+impl<T> PatternMut<T> for Tuple {
     for_tuples!(type Result = ( #( Tuple::Result ),* ););
-    fn match_mut(&self, expr: &mut Expr) -> Option<Self::Result> {
+    fn match_mut(&self, expr: &mut T) -> Option<Self::Result> {
         Some(for_tuples!((
             #( self.Tuple.match_mut(expr)? ),*
         )))
     }
 }
 #[impl_for_tuples(1, 12)]
-impl Pattern for Tuple {
-    fn r#match(&self, expr: &Expr) -> Option<Self::Result> {
+impl<T> Pattern<T> for Tuple {
+    fn r#match(&self, expr: &T) -> Option<Self::Result> {
         Some(for_tuples!((
             #( self.Tuple.r#match(expr)? ),*
         )))
